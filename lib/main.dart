423af93ac82c6c071018ff053defbe121310f63e
abdulaziz <alzaabi98@gmail.com>
2020-02-03 09:22:46 +0400

    the solution

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<String> images = [
+    "images/animal1.jpg",
+    "images/animal2.jpg",
+    "images/animal3.jpg",
+    "images/animal4.jpg",
+  ];
+
+  var imageIndex = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('Plain Project'),
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Image(
+              image: AssetImage(
+                images[imageIndex],
+              ),
+              fit: BoxFit.cover,
+            ),
+            height: 400,
+            width: double.infinity,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.arrow_back),
+                iconSize: 42.0,
+                color: Colors.pink,
+                onPressed: () {
+                  setState(() {
+                    imageIndex > 0
+                        ? imageIndex--
+                        : imageIndex = images.length - 1;
+                  });
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.arrow_forward),
+                iconSize: 42.0,
+                color: Colors.pink,
+                onPressed: () {
+                  setState(() {
+                    imageIndex < images.length - 1
+                        ? imageIndex++
+                        : imageIndex = 0;
+                  });
+                },
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
